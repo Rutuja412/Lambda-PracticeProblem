c55b45fb779ad667b07f4546e578ba6c11a0af87
Rutuja412 <rutujakadam4545@gmail.com>
2023-04-08 19:36:09 +0530

    [Rutuja]:Welcome

---
+++ Lambda_Practice_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lambda_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Lambda_Practice_Problem!");
+        }
+    }
+}
